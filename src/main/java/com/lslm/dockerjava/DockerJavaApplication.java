637f598e9f0e58cf8d96bf708c4b72a0606ffa9d
Lucas S <lucas.santos.silva2011@gmail.com>
2023-04-26 20:02:57 -0300

    First commit

---
+++ src/main/java/com/lslm/dockerjava/DockerJavaApplication.java
@@ -0,0 +1,13 @@
+package com.lslm.dockerjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lslm/dockerjava/DockerJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lslm.dockerjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
