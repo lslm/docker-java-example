c385b7a06d6ee5107144d9eb22321ec6a41f0821
Lucas <lucas.santos.silva2011@gmail.com>
2023-05-03 18:19:55 -0300

    Created User model and repository

--- src/main/java/com/lslm/dockerjava/controllers/UserRepository.java
+++ src/main/java/com/lslm/dockerjava/controllers/UserRepository.java
@@ -1,2 +1,10 @@
-package com.lslm.dockerjava.controllers;public class UserRepository {
+package com.lslm.dockerjava.controllers;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
 }
